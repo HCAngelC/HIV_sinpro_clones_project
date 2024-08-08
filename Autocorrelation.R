e112382b5dd0a59c0959eb27ac516db84803577a
Heng-Chang Chen <heng-chang.chen@port.lukasiewicz.gov.pl>
2024-08-08 13:37:13 +0200

    Update Autocorrelation.R

--- Autocorrelation.R
+++ Autocorrelation.R
@@ -10,9 +10,9 @@
 autocorrelation <- function(df) {
   df.pacf <- pacf(df$GFP_pos, plot = F)
   df.pacf <- data.frame(df.pacf$acf)
-  names(df.pacf) <- c("gfp_lag")
+  names(df.pacf) <- c("gfp_autocor")
   df.pacf <- df.pacf %>% dplyr::mutate(lag = c(1:10))
-  df_ <- df %>% dplyr::mutate(cv_lag = CV_FITC - lag(CV_FITC), sd_lag = SD_FITC - lag(SD_FITC), mean_lag = Mean_FITC - lag(Mean_FITC), median_lag = Median_FITC - lag(Median_FITC)) %>% dplyr::select(cv_lag, sd_lag, mean_lag, median_lag)
+  df_ <- df %>% dplyr::mutate(gfp_pos_lag = lag(GFP_pos), cv_lag = CV_FITC - lag(CV_FITC), sd_lag = SD_FITC - lag(SD_FITC), mean_lag = Mean_FITC - lag(Mean_FITC), median_lag = Median_FITC - lag(Median_FITC)) %>% dplyr::select(gfp_pos_lag, cv_lag, sd_lag, mean_lag, median_lag)
   df_ <- df_[2:11, ]
   df_ <- df_ %>% dplyr::mutate(lag = c(1:10))
   df_fi <- dplyr::inner_join(df_, df.pacf, by = "lag")
