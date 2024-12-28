9c31e0cc46badb457863ec2d6567191046a56231
Heng-Chang Chen <heng-chang.chen@port.lukasiewicz.gov.pl>
2024-12-28 19:10:14 +0100

    Create Calculation_of_proportionality_factor_c.R

---
+++ Calculation_of_proportionality_factor_c.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+rm(list=ls())
+##############################################################
+# Author: Heng-Chang Chen
+# Date: 2024.12.28
+##############################################################
+# Input: Parameters: qPCR and dPCR measures of sense- and antisense RNA and corresponding (%) GFP-positive cells, population-based CV and the autocorrelation time per sinpro clone.
+# Object: Calculation of the proportionality factor (C)
+##############################################################
+
+# R functions
+
+cal_parameter_c_pb.live.cv.qPCR <- function(df) {
+  c_sRNA <- data.frame((df$GFP_pos)/(df$sRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)))
+  names(c_sRNA) <- c("parameter_c")
+  c_asRNA <- data.frame((df$GFP_pos)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR)))
+  names(c_asRNA) <- c("parameter_c")
+  c_RNAs_sub <- data.frame(((df$GFP_pos)/(df$sRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)))-((df$GFP_pos)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR))))
+  names(c_RNAs_sub) <- c("parameter_c")
+  c_RNAs <- data.frame((df$GFP_pos)/(df$sRNA.qPCR - df$asRNA.qPCR)*(1/df$live_noise_T1.2/((1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)-(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR))))
+  names(c_RNAs) <- c("parameter_c")
+  
+  df <- bind_rows(c_sRNA, c_asRNA, c_RNAs_sub, c_RNAs)
+  df$type <- c(rep("sRNA", 8), rep("asRNA", 8), rep("subtraction", 8), rep("reshaped", 8))
+  df$group <- rep(c("S", "S", "A_min", "A_min", "S", "A_min", "A", "S"), 4)
+  df$clone <- rep(c("778", "788", "789", "795", "800", "802", "812", "815"), 4)
+  df$method <- c(rep("qPCR", 32))
+  
+  return(df)
+}
+
+cal_parameter_c_pb.live.cv.dPCR <- function(df) {
+  c_sRNA <- data.frame((df$GFP_pos)/(df$sRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)))
+  names(c_sRNA) <- c("parameter_c")
+  c_asRNA <- data.frame((df$GFP_pos)/(df$asRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR)))
+  names(c_asRNA) <- c("parameter_c")
+  c_RNAs_sub <- data.frame(((df$GFP_pos)/(df$sRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)))-((df$GFP_pos)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR))))
+  names(c_RNAs_sub) <- c("parameter_c")
+  c_RNAs <- data.frame((df$GFP_pos)/(df$sRNA.dPCR - df$asRNA.dPCR)*(1/df$live_noise_T1.2/((1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)-(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR))))
+  names(c_RNAs) <- c("parameter_c")
+  
+  df <- bind_rows(c_sRNA, c_asRNA, c_RNAs_sub, c_RNAs)
+  df$type <- c(rep("sRNA", 8), rep("asRNA", 8), rep("subtraction", 8), rep("reshaped", 8))
+  df$group <- rep(c("S", "S", "A_min", "A_min", "S", "A_min", "A", "S"), 4)
+  df$clone <- rep(c("778", "788", "789", "795", "800", "802", "812", "815"), 4)
+  df$method <- c(rep("dPCR", 32))
+  
+  return(df)
+}
