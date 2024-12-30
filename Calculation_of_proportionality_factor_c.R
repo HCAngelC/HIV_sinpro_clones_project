5be930a7db2b3565913292659c627a4e1123a7b8
Heng-Chang Chen <heng-chang.chen@port.lukasiewicz.gov.pl>
2024-12-30 14:48:06 +0100

    Update Calculation_of_proportionality_factor_c.R

--- Calculation_of_proportionality_factor_c.R
+++ Calculation_of_proportionality_factor_c.R
@@ -8,16 +8,26 @@ rm(list=ls())
 # Object: Calculation of the proportionality factor (C)
 ##############################################################
 
+## Mean fluoresence intensity (MFI)
+Table_S2 <- read.table("/Users/hchen/Documents/Praca/Projekten/Moi/HIV/Sensitized_HIV_latency_model/df_wichtig/Noise_space/sinpro_clones/Table_S2_noise_space_2025_v3.csv", header = T, stringsAsFactors = F)
+
+group_A <- Table_S2 %>% dplyr::filter(group == "A")
+MFI.groupA <- mean(group_A$Mean.live)
+group_Am <- Table_S2 %>% dplyr::filter(group == "A-")
+MFI.groupAm <- mean(group_Am$Mean.live)
+group_S <- Table_S2 %>% dplyr::filter(group == "S")
+MFI.groupS <- mean(group_S$Mean.live)
+
 # R functions
 
 cal_parameter_c_pb.live.cv.qPCR <- function(df) {
-  c_sRNA <- data.frame((df$GFP_pos)/(df$sRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)))
+  c_sRNA <- data.frame((df$MFI)/(df$sRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)))
   names(c_sRNA) <- c("parameter_c")
-  c_asRNA <- data.frame((df$GFP_pos)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR)))
+  c_asRNA <- data.frame((df$MFI)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR)))
   names(c_asRNA) <- c("parameter_c")
-  c_RNAs_sub <- data.frame(((df$GFP_pos)/(df$sRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)))-((df$GFP_pos)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR))))
+  c_RNAs_sub <- data.frame(((df$MFI)/(df$sRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)))-((df$MFI)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR))))
   names(c_RNAs_sub) <- c("parameter_c")
-  c_RNAs <- data.frame((df$GFP_pos)/(df$sRNA.qPCR - df$asRNA.qPCR)*(1/df$live_noise_T1.2/((1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)-(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR))))
+  c_RNAs <- data.frame((df$MFI)/(df$sRNA.qPCR - df$asRNA.qPCR)*(1/df$live_noise_T1.2/((1/df$live_noise_T1.2 + 1/df$tau.sRNA.qPCR)-(1/df$live_noise_T1.2 + 1/df$tau.asRNA.qPCR))))
   names(c_RNAs) <- c("parameter_c")
   
   df <- bind_rows(c_sRNA, c_asRNA, c_RNAs_sub, c_RNAs)
@@ -30,13 +40,13 @@ cal_parameter_c_pb.live.cv.qPCR <- function(df) {
 }
 
 cal_parameter_c_pb.live.cv.dPCR <- function(df) {
-  c_sRNA <- data.frame((df$GFP_pos)/(df$sRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)))
+  c_sRNA <- data.frame((df$MFI)/(df$sRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)))
   names(c_sRNA) <- c("parameter_c")
-  c_asRNA <- data.frame((df$GFP_pos)/(df$asRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR)))
+  c_asRNA <- data.frame((df$MFI)/(df$asRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR)))
   names(c_asRNA) <- c("parameter_c")
-  c_RNAs_sub <- data.frame(((df$GFP_pos)/(df$sRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)))-((df$GFP_pos)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR))))
+  c_RNAs_sub <- data.frame(((df$MFI)/(df$sRNA.dPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)))-((df$MFI)/(df$asRNA.qPCR)*(1/df$live_noise_T1.2/(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR))))
   names(c_RNAs_sub) <- c("parameter_c")
-  c_RNAs <- data.frame((df$GFP_pos)/(df$sRNA.dPCR - df$asRNA.dPCR)*(1/df$live_noise_T1.2/((1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)-(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR))))
+  c_RNAs <- data.frame((df$MFI)/(df$sRNA.dPCR - df$asRNA.dPCR)*(1/df$live_noise_T1.2/((1/df$live_noise_T1.2 + 1/df$tau.sRNA.dPCR)-(1/df$live_noise_T1.2 + 1/df$tau.asRNA.dPCR))))
   names(c_RNAs) <- c("parameter_c")
   
   df <- bind_rows(c_sRNA, c_asRNA, c_RNAs_sub, c_RNAs)
